7db4ba1c458aff4eee558d27c9e7b84af6401596
JinhoYoo <jinho.yoo@theloop.co.kr>
2019-12-16 10:51:22 +0900

    moved the position of about page.

--- frontend/todoapp/components/Layout.tsx
+++ frontend/todoapp/components/Layout.tsx
@@ -1,47 +1,46 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import * as React from "react";
+import Link from "next/link";
+import Head from "next/head";
 
 type Props = {
-  title?: string
-}
+  title?: string;
+};
 
 const Layout: React.FunctionComponent<Props> = ({
   children,
-  title = 'This is the default title',
+  title = "This is the default title"
 }) => (
-    <div>
-      <Head>
-        <title>{title}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <header>
-        <nav>
-          <Link href="/">
-            <a>Home</a>
-          </Link>{' '}
-          |{' '}
-          <Link href="/about">
-            <a>About</a>
-          </Link>{' '}
-          |{' '}
-          <Link href="/todo">
-            <a>Todo</a>
-          </Link>{' '}
-          |{' '}
-          <Link href="/users">
-            <a>Users List</a>
-          </Link>
+  <div>
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <header>
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>{" "}
+        |{" "}
+        <Link href="/todo">
+          <a>Todo</a>
+        </Link>{" "}
+        |{" "}
+        <Link href="/users">
+          <a>Users List</a>
+        </Link>{" "}
+        |{" "}
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </nav>
+    </header>
+    {children}
+    <footer>
+      <hr />
+      <span>I'm here to stay (Footer)</span>
+    </footer>
+  </div>
+);
 
-        </nav>
-      </header>
-      {children}
-      <footer>
-        <hr />
-        <span>I'm here to stay (Footer)</span>
-      </footer>
-    </div>
-  )
-
-export default Layout
+export default Layout;
